896cd854638ab3537f71de8f167ae28826ec64aa
J-Bakuli <julia.bakuli@yandex.ru>
2026-04-30 11:29:15 +0300

    Add tests for OngoingMatchDao

---
+++ src/test/java/OngoingMatchDaoTest.java
@@ -0,0 +1,58 @@
+import dao.InMemoryOngoingMatchDao;
+import dao.OngoingMatchDao;
+import exception.NotFoundException;
+import exception.ValidationException;
+import model.OngoingMatch;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+public class OngoingMatchDaoTest {
+    private OngoingMatchDao ongoingMatchDao;
+
+    @BeforeEach
+    public void setUp() {
+        ongoingMatchDao = new InMemoryOngoingMatchDao();
+    }
+
+    @Test
+    void saveOngoingMatchTest() {
+        UUID uuid = UUID.randomUUID();
+        OngoingMatch saved = ongoingMatchDao.save(new OngoingMatch(uuid, 1, 2, null));
+
+        Assertions.assertEquals(uuid, saved.getUuid());
+        Assertions.assertEquals(1, saved.getPlayer1());
+        Assertions.assertEquals(2, saved.getPlayer2());
+    }
+
+    @Test
+    void findByUuidTest() {
+        UUID uuid = UUID.randomUUID();
+        ongoingMatchDao.save(new OngoingMatch(uuid, 1, 2, null));
+
+        OngoingMatch found = ongoingMatchDao.findByUuid(uuid);
+        Assertions.assertEquals(uuid, found.getUuid());
+        Assertions.assertEquals(1, found.getPlayer1());
+        Assertions.assertEquals(2, found.getPlayer2());
+
+        Assertions.assertThrows(
+                ValidationException.class,
+                () -> ongoingMatchDao.findByUuid(UUID.randomUUID())
+        );
+    }
+
+    @Test
+    void removeByUuidTest() {
+        UUID uuid = UUID.randomUUID();
+        ongoingMatchDao.save(new OngoingMatch(uuid, 1, 2, null));
+
+        ongoingMatchDao.removeByUuid(uuid);
+
+        Assertions.assertThrows(
+                NotFoundException.class,
+                () -> ongoingMatchDao.removeByUuid(uuid)
+        );
+    }
+}
