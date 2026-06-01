7469ae9f7ed3b62a7c263dc7c4c54c7cdaec190b
J-Bakuli <julia.bakuli@yandex.ru>
2026-06-01 09:46:51 +0300

    Add FinishedMatchDto

---
+++ src/main/java/dto/FinishedMatchDto.java
@@ -0,0 +1,11 @@
+package dto;
+
+import java.time.LocalDateTime;
+
+public record FinishedMatchDto(
+        String player1Name,
+        String player2Name,
+        String winnerName,
+        LocalDateTime finishedAt
+) {
+}
