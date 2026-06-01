e1c0e764d1854c8b5e4a6394b1c7657a829aec3a
J-Bakuli <julia.bakuli@yandex.ru>
2026-06-01 09:55:16 +0300

    Update MatchesServlet#doGet for matches list rendering

--- src/main/java/controller/MatchesServlet.java
+++ src/main/java/controller/MatchesServlet.java
@@ -1,7 +1,9 @@
 package controller;
 
 import dao.MatchesDao;
+import dao.PlayerDao;
 import db.AppLifecycleListener;
+import dto.FinishedMatchDto;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import service.FinishedMatchesService;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet({"/matches"})
 @Slf4j
@@ -19,12 +22,15 @@ public class MatchesServlet extends BaseServlet {
     @Override
     public void init() throws ServletException {
         MatchesDao matchesDao = getRequiredAttribute(AppLifecycleListener.MATCHES_DAO_ATTR, MatchesDao.class);
-        finishedMatchesService = new FinishedMatchesService(matchesDao);
+        PlayerDao playerDao = getRequiredAttribute(AppLifecycleListener.PLAYER_DAO_ATTR, PlayerDao.class);
+        finishedMatchesService = new FinishedMatchesService(matchesDao, playerDao);
     }
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.info("GET /matches");
+        List<FinishedMatchDto> finishedMatches = finishedMatchesService.getAllFinishedMatches();
+        req.setAttribute("matches", finishedMatches);
         req.getRequestDispatcher("/WEB-INF/views/matches.jsp").forward(req, resp);
     }
 }
