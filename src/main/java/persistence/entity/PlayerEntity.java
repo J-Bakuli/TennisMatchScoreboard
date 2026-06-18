63f03ae052568a6bbb8601b0897448f75f74cf2f
J-Bakuli <julia.bakuli@yandex.ru>
2026-06-18 12:11:49 +0300

    Refactor PlayerEntity to use safe constructors

--- src/main/java/persistence/entity/PlayerEntity.java
+++ src/main/java/persistence/entity/PlayerEntity.java
@@ -5,20 +5,26 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Setter
+import java.util.Objects;
+
 @Getter
-@NoArgsConstructor
 @Entity
-@Table(name = "players")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "players", indexes = @Index(name = "idx_players_name", columnList = "name"))
 public class PlayerEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(name = "name", nullable = false, unique = true, length = 20)
     private String name;
+
+    public PlayerEntity(String name) {
+        this.name = Objects.requireNonNull(name);
+    }
 }
