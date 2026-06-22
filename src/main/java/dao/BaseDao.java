7e9f6ac0e3544a351eb075691af1dd202640b39c
J-Bakuli <julia.bakuli@yandex.ru>
2026-06-22 12:22:22 +0300

    Remove BaseDao

--- src/main/java/dao/BaseDao.java
+++
@@ -1,5 +0,0 @@
-package dao;
-
-public interface BaseDao<T> {
-    T save(T entity);
-}
--- src/main/java/dao/PlayerDao.java
+++ src/main/java/dao/PlayerDao.java
@@ -2,7 +2,8 @@ package dao;
 
 import model.Player;
 
-public interface PlayerDao extends BaseDao<Player> {
+public interface PlayerDao {
     Player findByName(String name);
     Player findById(Integer id);
+    Player save(Player player);
 }
